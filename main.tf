2641df8f815a2669c9f158810856602760819b8e
Fernando Hernández San Felipe <ironwolphern@iWolphPortPRO.local>
2024-11-13 21:15:43 +0100

    Modify policies and outputs

--- main.tf
+++ main.tf
@@ -11,6 +11,16 @@ resource "vault_policy" "devops" {
   policy = file("${path.module}/policy/devops-policy.hcl")
 }
 
+resource "vault_policy" "cicd-ro" {
+  name   = "cicd-ro"
+  policy = file("${path.module}/policy/approle-cicd-ro-policy.hcl")
+}
+
+resource "vault_policy" "cicd-rw" {
+  name   = "cicd-rw"
+  policy = file("${path.module}/policy/approle-cicd-rw-policy.hcl")
+}
+
 # Uncomment the following block to enable the Vault authentication method userpass
 resource "vault_auth_backend" "userpass" {
   type = "userpass"
@@ -23,18 +33,11 @@ resource "vault_generic_endpoint" "admin_user" {
   ignore_absent_fields = true
 
   data_json = jsonencode({
-    policies  = [vault_policy.admin.name]
-    password  = var.admin_password
-    token_ttl = "1h"
+    token_policies    = [vault_policy.admin.name]
+    password          = var.admin_password
+    token_ttl         = "1h"
+    token_bound_cidrs = var.ip_whitelist
   })
-}
-
-output "admin_username" {
-  value = "admin"
-}
-
-output "admin_policy_name" {
-  value = vault_policy.admin.name
 }
 
 resource "vault_generic_endpoint" "devops_user" {
@@ -43,28 +46,11 @@ resource "vault_generic_endpoint" "devops_user" {
   ignore_absent_fields = true
 
   data_json = jsonencode({
-    policies  = [vault_policy.devops.name]
-    password  = var.devops_password
-    token_ttl = "1h"
+    token_policies    = [vault_policy.devops.name]
+    password          = var.devops_password
+    token_ttl         = "1h"
+    token_bound_cidrs = var.ip_whitelist
   })
-}
-
-output "devops_username" {
-  value = "devops"
-}
-
-output "devops_policy_name" {
-  value = vault_policy.devops.name
-}
-
-resource "vault_policy" "kestra-ro" {
-  name   = "kestra-ro"
-  policy = file("${path.module}/policy/approle-kestra-ro-policy.hcl")
-}
-
-resource "vault_policy" "kestra-rw" {
-  name   = "kestra-rw"
-  policy = file("${path.module}/policy/approle-kestra-rw-policy.hcl")
 }
 
 # Uncomment the following block to enable the Vault AppRole authentication backend
@@ -73,10 +59,31 @@ resource "vault_auth_backend" "approle" {
   path = "approle"
 }
 
-resource "vault_approle_auth_backend_role" "kestra-ro" {
+resource "vault_approle_auth_backend_role" "cicd-ro" {
   backend        = vault_auth_backend.approle.path
-  role_name      = "kestra-ro"
-  token_policies = [vault_policy.kestra-ro.name]
+  role_name      = "cicd-ro"
+  token_policies = [vault_policy.cicd-ro.name]
+
+  # AppRole security configuration
+  token_ttl             = 3600
+  token_max_ttl         = 14400
+  token_num_uses        = 0
+  secret_id_ttl         = 300
+  secret_id_num_uses    = 0
+  bind_secret_id        = true
+  secret_id_bound_cidrs = var.ip_whitelist
+  token_bound_cidrs     = var.ip_whitelist
+}
+
+resource "vault_approle_auth_backend_role_secret_id" "cicd-ro" {
+  backend   = vault_auth_backend.approle.path
+  role_name = vault_approle_auth_backend_role.cicd-ro.role_name
+}
+
+resource "vault_approle_auth_backend_role" "cicd-rw" {
+  backend        = vault_auth_backend.approle.path
+  role_name      = "cicd-rw"
+  token_policies = [vault_policy.cicd-rw.name]
 
   # AppRole security configuration
   token_ttl             = 3600
@@ -89,50 +96,9 @@ resource "vault_approle_auth_backend_role" "kestra-ro" {
   token_bound_cidrs     = var.ip_whitelist
 }
 
-resource "vault_approle_auth_backend_role_secret_id" "kestra-ro" {
+resource "vault_approle_auth_backend_role_secret_id" "cicd-rw" {
   backend   = vault_auth_backend.approle.path
-  role_name = vault_approle_auth_backend_role.kestra-ro.role_name
-}
-
-output "role_id_ro" {
-  value     = vault_approle_auth_backend_role.kestra-ro.role_id
-  sensitive = true
-}
-
-output "secret_id_ro" {
-  value     = vault_approle_auth_backend_role_secret_id.kestra-ro.secret_id
-  sensitive = true
-}
-
-resource "vault_approle_auth_backend_role" "kestra-rw" {
-  backend        = vault_auth_backend.approle.path
-  role_name      = "kestra-rw"
-  token_policies = [vault_policy.kestra-rw.name]
-
-  # AppRole security configuration
-  token_ttl             = 3600
-  token_max_ttl         = 14400
-  token_num_uses        = 0
-  secret_id_ttl         = 1800
-  secret_id_num_uses    = 0
-  bind_secret_id        = true
-  secret_id_bound_cidrs = var.ip_whitelist
-  token_bound_cidrs     = var.ip_whitelist
-}
-
-resource "vault_approle_auth_backend_role_secret_id" "kestra-rw" {
-  backend   = vault_auth_backend.approle.path
-  role_name = vault_approle_auth_backend_role.kestra-rw.role_name
-}
-
-output "role_id_rw" {
-  value     = vault_approle_auth_backend_role.kestra-rw.role_id
-  sensitive = true
-}
-
-output "secret_id_rw" {
-  value     = vault_approle_auth_backend_role_secret_id.kestra-rw.secret_id
-  sensitive = true
+  role_name = vault_approle_auth_backend_role.cicd-rw.role_name
 }
 
 # Uncomment the following block to enable the Vault engine kv-v2
@@ -146,8 +112,7 @@ resource "vault_mount" "laboratory" {
 resource "vault_kv_secret_v2" "ssh" {
   depends_on = [vault_mount.laboratory]
   count      = (var.ssh_keys != null) ? 1 : 0
-  #mount               = vault_mount.laboratory.path
-  mount               = "laboratory"
+  mount               = vault_mount.laboratory.path
   name                = "ssh"
   cas                 = 1
   delete_all_versions = true
@@ -164,8 +129,7 @@ resource "vault_kv_secret_v2" "ssh" {
 resource "vault_kv_secret_v2" "pve_auth_user_password" {
   depends_on = [vault_mount.laboratory]
   count      = (var.pve_auth_keys_userpass != null) ? 1 : 0
-  #mount               = vault_mount.laboratory.path
-  mount               = "laboratory"
+  mount               = vault_mount.laboratory.path
   name                = "proxmox/auth/userpass"
   cas                 = 1
   delete_all_versions = true
@@ -182,8 +146,7 @@ resource "vault_kv_secret_v2" "pve_auth_user_password" {
 resource "vault_kv_secret_v2" "pve_auth_user_api_token" {
   depends_on = [vault_mount.laboratory]
   count      = (var.pve_auth_keys_api != null) ? 1 : 0
-  #mount               = vault_mount.laboratory.path
-  mount               = "laboratory"
+  mount               = vault_mount.laboratory.path
   name                = "proxmox/auth/api"
   cas                 = 1
   delete_all_versions = true
--- policy/approle-cicd-rw-policy.hcl
+++ policy/approle-cicd-rw-policy.hcl
@@ -1,12 +1,12 @@
 # Read-Write permission on secrets stored at 'laboratory/data/*'
 path "laboratory/data/*" {
-  capabilities = ["create", "read", "update", "delete", "list"]
+  capabilities = ["create", "read", "update", "list"]
 }
 path "laboratory/metadata/*" {
-  capabilities = ["create", "read", "update", "list", "delete", "sudo"]
+  capabilities = ["create", "read", "update", "list"]
 }
 
 # Manage all AppRoles Secrets IDs
-path "auth/approle/role/kestra-ro/secret-id" {
+path "auth/approle/role/cicd-ro/secret-id" {
   capabilities = ["create", "read", "update", "list"]
 }
